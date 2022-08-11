aa252133c34cd65a44992265e5b1a073cc6917bf
Victor Goya <victor@mooveo.co>
2022-08-11 10:02:42 +0200

    fix stuff

--- pages/index.js
+++ pages/index.js
@@ -62,13 +62,13 @@ export default function Home() {
                   <div className="flex justify-center gap-6 mt-4">
                     <a href="https://www.facebook.com/lesbullesdepoils/">
                       <svg className="flex-shrink-0 w-6 h-6 text-gray-500 hover:text-gray-900" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                        xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" aria-hidden="true">
+                        xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                         <path d="M22.258 1H2.242C1.556 1 1 1.556 1 2.242v20.016c0 .686.556 1.242 1.242 1.242h10.776v-8.713h-2.932V11.39h2.932V8.887c0-2.906 1.775-4.489 4.367-4.489 1.242 0 2.31.093 2.62.134v3.037l-1.797.001c-1.41 0-1.683.67-1.683 1.653v2.168h3.362l-.438 3.396h-2.924V23.5h5.733c.686 0 1.242-.556 1.242-1.242V2.242C23.5 1.556 22.944 1 22.258 1" fill="currentColor" />
                       </svg>
                     </a>
                     <a href="https://twitter.com/bullesdepoils">
                       <svg className="flex-shrink-0 w-6 h-6 text-gray-500 hover:text-gray-900" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                        xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" aria-hidden="true">
+                        xmlns="http://www.w3.org/2000/svg"  aria-hidden="true">
                         <path d="M7.548 22.501c9.056 0 14.01-7.503 14.01-14.01 0-.213 0-.425-.015-.636A10.02 10.02 0 0024 5.305a9.828 9.828 0 01-2.828.776 4.94 4.94 0 002.165-2.724 9.867 9.867 0 01-3.127 1.195 4.929 4.929 0 00-8.391 4.491A13.98 13.98 0 011.67 3.9a4.928 4.928 0 001.525 6.573A4.887 4.887 0 01.96 9.855v.063a4.926 4.926 0 003.95 4.827 4.917 4.917 0 01-2.223.084 4.93 4.93 0 004.6 3.42A9.88 9.88 0 010 20.289a13.941 13.941 0 007.548 2.209" fill="currentColor" />
                       </svg>
                     </a>
